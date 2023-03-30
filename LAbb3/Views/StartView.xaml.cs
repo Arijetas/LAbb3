aa52fc7836f817c19c0b88c05f1307cf44459872
Arijetas <arijeta.sherifi@iths.se>
2023-03-30 20:00:18 +0200

    updated quiz

--- LAbb3/Views/StartView.xaml.cs
+++ LAbb3/Views/StartView.xaml.cs
@@ -1,11 +1,4 @@
 ﻿using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LAbb3.Views
 {
